49110b782fe184432e21d04817fd4b9eb05cd769
Sveta Apriamashvili <s.v.apriamashvili@yandex.com>
2021-08-25 12:18:06 +1000

    fix deleting likes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -78,7 +78,7 @@ function App() {
     }
 
     function handleCardLike(card) {
-        const isLiked = card.likes.some(i => i._id === currentUser._id);
+        const isLiked = card.likes.some(i => i === currentUser._id);
         Api.changeLikeCardStatus(card._id, isLiked)
             .then((newCard) => {
                 setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
