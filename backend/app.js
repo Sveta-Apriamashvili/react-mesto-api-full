e04f86653934a16daed52fb5ab64d06ef6ba6202
Sveta Apriamashvili <s.v.apriamashvili@yandex.com>
2021-08-30 16:23:07 +1000

    remove settings for crash-test

--- backend/app.js
+++ backend/app.js
@@ -40,11 +40,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 app.use(express.json());
 app.use(requestLogger);
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
